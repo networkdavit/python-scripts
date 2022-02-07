17785fd947bf9245405039d00f1d1356a7266407
networkdavit <lonecreation1612@gmail.com>
2022-02-07 13:01:50 +0400

    Adding a subdomain scanner

---
+++ subscan.py
@@ -0,0 +1,30 @@
+import requests 
+import getopt
+import sys
+
+if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+	print("Welcome to subdomain scanner")
+	print("-------------------------------------")
+	print("A typical attack looks like this: python3 domain subdomain_wordlist.txt output.txt")
+	sys.exit()
+
+try:
+	domain = sys.argv[1]
+	wordlist = sys.argv[2]
+	output = sys.argv[3]
+except IndexError:
+	output = "output.txt"
+
+file = open(wordlist)
+content = file.read()
+subdomain_list = content.splitlines()
+
+for subdomain in subdomain_list:
+	url = f"https://{subdomain}.{domain}"
+	try:
+		r = requests.get(url, timeout=2)
+		print(f"FOUND {url}")
+		with open(output, "a") as f:
+			f.write(url + "\n")
+	except requests.ConnectionError:
+		pass
