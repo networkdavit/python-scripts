0e25e192f944eff6335f83a81f8b312b5d72d97c
networkdavit <65449065+networkdavit@users.noreply.github.com>
2024-07-06 17:01:26 +0400

    Create ip_location_finder.py

---
+++ ip_location_finder.py
@@ -0,0 +1,19 @@
+import requests
+import sys 
+
+def ip_geolocation(ip):
+    url = f"https://ipinfo.io/{ip}/json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        geo_data = response.json()
+        print(f"IP: {geo_data['ip']}")
+        print(f"City: {geo_data['city']}")
+        print(f"Region: {geo_data['region']}")
+        print(f"Country: {geo_data['country']}")
+        print(f"Org: {geo_data['org']}")
+    else:
+        print(f"Error: {response.status_code}")
+
+ip = sys.argv[1]
+ip_geolocation(ip)
+                   
